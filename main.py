1b6a00b48fb8827710ca2cf0ea9842b45f3a3862
Mark <bordumar@gmail.com>
2014-10-01 15:57:32 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# importing wx files
+import wx
+ 
+# import the newly created GUI file
+import maingui
+
+
+# inherit from the MainFrame created in wxFowmBuilder and create CalcFrame
+class CalcFrame(maingui.Mainframe):
+    # constructor
+    def __init__(self, parent):
+        # initialize parent class
+        maingui.Mainframe.__init__(self, parent)
+ 
+    # what to when 'Solve' is clicked
+    # wx calls this function with and 'event' object
+    def doleftup(self, event):
+        try:
+			print 'test'
+        except Exception:
+            print 'error'
+ 
+# mandatory in wx, create an app, False stands for not deteriction stdin/stdout
+# refer manual for details
+app = wx.App(False)
+ 
+# create an object of CalcFrame
+frame = CalcFrame(None)
+# show the frame
+frame.Show(True)
+# start the applications
+app.MainLoop()
---
+++ maingui.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*- 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Oct  1 2014)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO "NOT" EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class Mainframe
+###########################################################################
+
+class Mainframe ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 574,394 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		bSizer2 = wx.BoxSizer( wx.VERTICAL )
+		
+		bSizer4 = wx.BoxSizer( wx.VERTICAL )
+		
+		
+		bSizer4.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
+		
+		bSizer7 = wx.BoxSizer( wx.VERTICAL )
+		
+		
+		bSizer4.Add( bSizer7, 1, wx.EXPAND, 5 )
+		
+		
+		bSizer2.Add( bSizer4, 1, wx.EXPAND, 5 )
+		
+		bSizer6 = wx.BoxSizer( wx.VERTICAL )
+		
+		
+		bSizer6.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
+		
+		self.m_button8 = wx.Button( self, wx.ID_ANY, u"Test", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.m_button8, 0, wx.ALL, 5 )
+		
+		
+		bSizer2.Add( bSizer6, 1, wx.EXPAND, 5 )
+		
+		
+		self.SetSizer( bSizer2 )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.m_button8.Bind( wx.EVT_LEFT_UP, self.doleftup )
+	
+	def __del__( self ):
+		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def doleftup( self, event ):
+		event.Skip()
+	
+
