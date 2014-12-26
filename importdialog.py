877bf465b14c8cb8ec27d37f4e10e979bcb8c7e1
labordus <bordumar@gmail.com>
2014-12-27 00:51:01 +0100

    clean-up

--- importdialog.py
+++ importdialog.py
@@ -2,23 +2,9 @@
 # -*- coding: ISO-8859-1 -*-
 
 import wx
-import os
-import imp
 import sys
 import maingui
 from db import ImportJSON2DB, SetImported
-
-
-def main_is_frozen():
-    return (hasattr(sys, "frozen") or  # new py2exe
-            hasattr(sys, "importers")  # old py2exe
-            or imp.is_frozen("__main__"))  # tools/freeze
-
-
-def get_main_dir():
-    if main_is_frozen():
-        return os.path.dirname(sys.executable)
-    return os.path.dirname(sys.argv[0])
 
 
 class ImportDialog(maingui.dlgImport):
