92bac46b1d6d7b0a849402b72f378af65c1179b2
labordus <bordumar@gmail.com>
2015-01-10 23:01:29 +0100

    Oude website teruggehaald, dus ook de HTTPserver

--- archiveview.py
+++ archiveview.py
@@ -1,11 +1,15 @@
 import wx
 import wx.html2
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import threading
+from time import sleep
 import os
 
 oudepad = os.getcwd()
 
 
-class MyBrowser(wx.Dialog):
+class MyBrowserNew(wx.Dialog):
 
     def __init__(self, *args, **kwds):
         wx.Dialog.__init__(self, *args, **kwds)
@@ -23,10 +27,66 @@ class MyBrowser(wx.Dialog):
         # verwijder archive.html
         import appdirs
         path = appdirs.user_data_dir('aquaf', False, False, False)
-        filepath = os.path.join(path, 'archive.html')
+        filepath = os.path.join(path, 'archivenew.html')
         try:
             os.remove(filepath)
         except OSError:
             pass
 
         event.Skip()
+
+
+class MyServer():
+    HandlerClass = SimpleHTTPRequestHandler
+    ServerClass = BaseHTTPServer.HTTPServer
+    Protocol = "HTTP/1.0"
+    server_address = ("127.0.0.1", 8000)
+    HandlerClass.protocol_version = Protocol
+    httpd = ServerClass(server_address, HandlerClass)
+
+    def run(self):
+        #        sa = self.httpd.socket.getsockname()
+        #        print "Served HTTP on", sa[0], "port", sa[1], "..."
+        import appdirs
+        datapad = appdirs.user_data_dir('aquaf', False, False, False)
+        os.chdir(datapad)
+
+        thread = threading.Thread(target=self.httpd.serve_forever)
+        thread.deamon = True
+        thread.start()
+        sleep(0.25)
+
+    def stop(self):
+        self.httpd.shutdown()
+        os.chdir(oudepad)
+
+
+class MyBrowser(wx.Dialog):
+
+    def __init__(self, *args, **kwds):
+        wx.Dialog.__init__(self, *args, **kwds)
+#        self.SetWindowStyleFlag(wx.RESIZE_BORDER)
+#        self.SetWindowStyle(wx.RESIZE_BORDER | wx.CAPTION)
+        self.SetWindowStyleFlag(wx.RESIZE_BORDER | wx.DEFAULT_DIALOG_STYLE | wx.MAXIMIZE_BOX)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.browser = wx.html2.WebView.New(self)
+        sizer.Add(self.browser, 1, wx.EXPAND, 10)
+        self.SetSizer(sizer)
+        self.SetSize((800, 800))
+        self.Bind(wx.EVT_CLOSE, self.oncloseMyBrowser)
+        serve = MyServer()
+        MyServer.run(serve)
+
+    def oncloseMyBrowser(self, event):
+        serve = MyServer()
+        MyServer.stop(serve)
+        event.Skip()
+
+# verwijder aquaf.json
+        import appdirs
+        path = appdirs.user_data_dir('aquaf', False, False, False)
+        filepath = os.path.join(path, 'aquaf.json')
+        try:
+            os.remove(filepath)
+        except OSError:
+            pass
