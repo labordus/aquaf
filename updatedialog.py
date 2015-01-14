aa69e1a6d6e70f4c2f8d0e931dd22c47c70cad53
labordus <bordumar@gmail.com>
2015-01-14 21:51:15 +0100

    datum toegevoegd aan update-dialog

--- updatedialog.py
+++ updatedialog.py
@@ -12,17 +12,20 @@ class Update(maingui.dlgUpdate):
         # initialize parent class
         maingui.dlgUpdate.__init__(self, parent)
 
-    def LoadText(self, sVersion, sReason):
+    def LoadText(self, ReleaseVersion, ReleaseDate, ReleaseChanges):
         text = self.textUpdate
         text.AppendText("Nieuwe versie : ")
-        text.SetDefaultStyle(wx.TextAttr('#008502'))
-        text.AppendText(str(sVersion) + '\n')
+        text.SetDefaultStyle(wx.TextAttr(wx.RED))
+        text.AppendText(str(ReleaseVersion) + '\n')
+        text.SetDefaultStyle(wx.TextAttr(wx.BLACK))
+        text.AppendText("Datum : ")
+        text.AppendText(str(ReleaseDate) + '\n')
         text.SetDefaultStyle(wx.TextAttr(wx.BLACK))
         text.AppendText("Veranderingen : \n")
-        for row in sReason:
+        for row in ReleaseChanges:
             #            sPoep = str((row[0]))
             sRegel = str(row)
-            text.AppendText('- ' + sRegel + '\n')
+            text.AppendText(sRegel + '\n')
 
     def onbtnAfsluitenClick(self, event):
         self.EndModal(wx.ID_OK)
